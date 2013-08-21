656acb66baa3baf237cec5a68537a286443c5764
Tom Szczesny <tavmem@gmail.com>
2013-08-21 13:59:13 -0400

    add includes, remove V1 & V2, fix gcc warnings

---
+++ incs.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>  // malloc
+#include <stdio.h>   // printf
+#include <string.h>  // strlen
+typedef char C;
+typedef long I;
+typedef void* V;
+typedef struct a{I t,r,d[3],p[2];}*A;
+#define P printf
+#define R return
+#define DO(n,x) {I i=0,_n=(n);for(;i<_n;++i){x;}}
