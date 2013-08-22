5cd395752433611fb2ddb665de7c62ef1391917b
Tom Szczesny <tavmem@gmail.com>
2013-08-22 11:28:28 -0400

    more diagnostics for Cygwin crash

--- ncn.c
+++ ncn.c
@@ -1,8 +1,27 @@
 #include "incs.h"
-I *ma(I n){R(I*)malloc(n*4);}
+I *ma(I n){
+  V *p=malloc((size_t)(n*4));
+  O("ma: p: %p\n",p);
+  R(I*)p;}
 V mv(I*d,I*s,I n){DO(n,d[i]=s[i]);}
-I tr(I r,I*d){I z=1;DO(r,z=z*d[i]);R z;}
-A ga(I t,I r,I*d){A z=(A)ma(5+tr(r,d));z->t=t,z->r=r,mv(z->d,d,r);R z;}
+I tr(I r,I*d){
+  O("tr: r,d: %ld %p\n",r,d);
+  I z=1;
+  O("tr-z-bef: %ld\n",z);
+  DO(r,z=z*d[i]);
+  O("tr-z-aft: %ld\n",z);
+  R z;}
+A ga(I t,I r,I*d){
+  O("ga: t,r,d: %ld %ld %p\n",t,r,d);
+  if(d==0x0) O("have 0x0\n");
+  O("5+tr(r,d): %ld\n",5+tr(r,d));
+  I *p=ma(5+tr(r,d));
+  O("p: %p\n",p);
+  A z=(A)p;
+  O("ga: z-bef: %p\n",z);
+  z->t=t,z->r=r,mv(z->d,d,r);
+  O("ga: z-aft: %p\n",z);
+  R z;}
 A iota(A w){I n=*w->p;A z=ga(0,1,&n);DO(n,z->p[i]=i);R z;}
 A plus(A a, A w){I r=w->r,*d=w->d,n=tr(r,d);A z=ga(0,r,d);
   DO(n,z->p[i]=a->p[i]+w->p[i]);R z;}
@@ -26,7 +45,9 @@ V pr(A w){I r=w->r,*d=w->d,n=tr(r,d);DO(r,pi(d[i]));nl();
 A (*vd[])()={0,plus,from,find,0,rsh,cat},
   (*vm[])()={0,id,size,iota,box,sha,0}; 
 I qp(I a){R  a>='a'&&a<='z';}
-I qv(I a){R a<'a';}
+I qv(I a){
+  O("a,'a': %ld %d\n",a,'a');
+  R a<'a';}
 C vt[]="+{~<#,"; I st[26];
 A ex(I *e){
   I a=*e;
@@ -36,13 +57,22 @@ A ex(I *e){
     a= st[ a-'a'];}
   O("qv(a): %ld\n",qv(a));
   R qv(a)?(*vm[a])(ex(e+1)):e[1]?(*vd[e[1]])(a,ex(e+2)):(A)a;}
-I noun(C c){A z;if(c<'0'||c>'9')R 0;z=ga(0,0,0);*z->p=c-'0';R(I)z;}
+I noun(C c){
+  O("c: %d\n",c);
+  A z;
+  if(c<'0'||c>'9')R 0;
+  z=ga(0,0,0);
+  *z->p=c-'0';
+  O("noun: z (I)z: %p %ld\n",z,(I)z);
+  R(I)z;}
 I verb(C c){I i=0;for(;vt[i];)if(vt[i++]==c)R i;R 0;}
 I *wd(C *s){
   I a,n=strlen(s),*e=ma(n+1);
   O("strlen: %ld\n",n);
   C c;
+  O("noun(s[0]): %ld\n",noun(s[0]));
   DO(n,e[i]=(a=noun(c=s[i]))?a:(a=verb(c))?a:c);e[n]=0;
+  O("e, *e: %p %ld\n",e,*e);
   R e;}
 
 int main(){C s[99];while(gets(s))pr(ex(wd(s)));R 0;}
